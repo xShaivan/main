80752c484ed4807c39b6a0c7cb58957f18f60648
Xian Hao <xianhao@u.nus.edu>
2018-09-19 20:10:19 +0800

    EditInfoCommand Feature - Created EditInfoCommand class - Updated AddressParser to accept editinfo - editinfo prints "COMMAND NOT YET IMPLEMENTED"

---
+++ src/main/java/seedu/address/logic/commands/EditInfoCommand.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Edits optional details of an existing patient in the address book.
+ */
+
+public class EditInfoCommand extends Command {
+    public static final String COMMAND_WORD = "editinfo";
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        throw new CommandException("COMMAND NOT YET IMPLEMENTED");
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -11,6 +11,7 @@ import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.EditInfoCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
@@ -83,6 +84,9 @@ public class AddressBookParser {
 
         case RedoCommand.COMMAND_WORD:
             return new RedoCommand();
+
+        case EditInfoCommand.COMMAND_WORD:
+            return new EditInfoCommand();
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
