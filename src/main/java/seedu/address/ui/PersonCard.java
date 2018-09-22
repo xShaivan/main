fcc10d338a9ca6a4ae29c45b9350408e5d83df7c
Hovan Tan Fang Hao <warshaivan@gmail.com>
2018-09-22 15:11:25 +0800

    UI: Connect MedHistory field to PersonCard
    Observation: GUI no longer shows EMPTY_MEDHISTORY which was $medhistory. Now it shows nothing.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -49,6 +49,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        medhistory.setText(person.getMedHistory().value);
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 
