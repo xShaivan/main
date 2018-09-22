60ac69bd6587d93c3f55e4c157aa8bd52f3d477b
Hovan Tan Fang Hao <warshaivan@gmail.com>
2018-09-22 19:24:17 +0800

    Logic: Implement AddHistCommand execute() logic
    This is the end of the tutorial to add a new command. In this commit, the error message got replaced with real logic to edit the medical history of a patient.

--- src/main/java/seedu/address/logic/commands/AddHistCommand.java
+++ src/main/java/seedu/address/logic/commands/AddHistCommand.java
@@ -3,11 +3,16 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_HISTORY;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.MedHistory;
 
 /**
@@ -23,7 +28,8 @@ public class AddHistCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_HISTORY + "Diabetes.";
 
-    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, MedHist: %2$s";
+    public static final String MESSAGE_ADD_MEDHISTORY_SUCCESS = "Added medical history to Person: %1$s";
+    public static final String MESSAGE_DELETE_MEDHISTORY_SUCCESS = "Removed medical history from Person: %1$s";
     private final Index index;
     private final MedHistory medhistory;
     /**
@@ -38,8 +44,28 @@ public class AddHistCommand extends Command {
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), medhistory));
+        List<Person> lastShownList = model.getFilteredPersonList();
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), medhistory, personToEdit.getTags());
+        model.updatePerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.commitAddressBook();
+        return new CommandResult(generateSuccessMessage(editedPerson));
     }
+
+    /**
+     * Generates a command execution success message based on whether the medical history is added to or removed from
+     * {@code personToEdit}.
+     */
+    private String generateSuccessMessage(Person personToEdit) {
+        String message = !medhistory.value.isEmpty() ? MESSAGE_ADD_MEDHISTORY_SUCCESS : MESSAGE_DELETE_MEDHISTORY_SUCCESS;
+        return String.format(message, personToEdit);
+    }
+
     @Override
     public boolean equals(Object other) {
         // short circuit if same object
