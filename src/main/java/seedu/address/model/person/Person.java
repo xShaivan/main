e509b82dafe3fd523eb05671dd99dbe07cf409d3
Hovan Tan Fang Hao <warshaivan@gmail.com>
2018-09-22 13:57:34 +0800

    Model: Modify Person Class to support MedHistory Field
    Add and Edit Command for MedHistory does not work here yet.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -22,17 +22,19 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final MedHistory medhistory;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, MedHistory medhistory, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.medhistory = medhistory;
         this.tags.addAll(tags);
     }
 
@@ -50,6 +52,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public MedHistory getMedHistory() {
+        return medhistory;
     }
 
     /**
