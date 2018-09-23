b7964723b033277ca275ee2e1b977b14634a8377
Chew Kah Meng <kahmeng@comp.nus.edu.sg>
2018-09-23 22:48:15 +0800

    Rectified some style mistakes

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -10,11 +10,11 @@ import java.util.stream.Collectors;
 import javax.xml.bind.annotation.XmlElement;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.medicalreport.MedicalReport;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.medicalreport.MedicalReport;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
