b25292a8920ee5aa5aae1cff5ee461c3f26b9bfe
brandonccm1996 <brandonccm@hotmail.com>
2018-09-25 00:27:12 +0800

    Ui: Connect Appt field to PersonCard
    - Just modify PersonCard's constructor to add the new method.
    - For test, modify GuiTestAssert assertCardDisplaysPerson(...) to
    compare the now-functioning appt label.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -49,6 +49,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        appt.setText(person.getAppt().value);
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 
