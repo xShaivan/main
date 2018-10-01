3d1d7b3d4d89e5a9500dfa17da6699430233b81f
Xian Hao <xianhao@u.nus.edu>
2018-10-01 22:53:33 +0800

    Update EditCommand.java

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -105,7 +105,7 @@ public class EditCommand extends Command {
         Nric updatedNric = personToEdit.getNric();
         Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
 
-        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedAppt, updated Nric, updatedTags);
+        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedAppt, updatedNric, updatedTags);
     }
 
     @Override
