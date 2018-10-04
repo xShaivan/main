6471e6efe384d95b059068f0bebd7922d33f9f68
brandonccm1996 <brandonccm@hotmail.com>
2018-10-04 23:06:46 +0800

    Modified some xml files to pass xml tests
    - For now, the xml tests require apptStart, apptEnd, apptVenue, apptInfo, apptName fields but these tests will have to be modified as this is not the intended effect of the final feature

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -42,7 +42,7 @@ public class TypicalPersons {
             .withEmail("lydia@example.com").withAddress("little tokyo").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street").withMedicalReport("Asthma")
-            .withMedHistory("Has Ebola.").withAppt("APPT_EXAMPLE1").withNric("S5185093I").build();
+            .withMedHistory("Has Ebola.").withAppt(APPT_EXAMPLE1).withNric("S5185093I").build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
