f4573701cb6e746cd04b1068fb49488939c45932
brandonccm1996 <brandonccm@hotmail.com>
2018-10-18 00:14:46 +0800

    Rectified mistakes

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -177,8 +177,6 @@ public class XmlAdaptedPerson {
 
         final Set<MedHistory> modelMedHistory = new HashSet<>(personMedHistories);
 
-        final MedHistory modelMedHistory = new MedHistory(modelMedHistDate, modelAllergy, modelPrevCountry);
-
         /**
          * ==================================================
          * APPT SUBFIELDS
