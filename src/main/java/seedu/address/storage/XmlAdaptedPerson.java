21e093104ac4503f53e1429f2145b4b68480ac2a
brandonccm1996 <brandonccm@hotmail.com>
2018-10-17 21:20:19 +0800

    Rectified checkstyle errors

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -57,7 +57,8 @@ public class XmlAdaptedPerson {
      * Constructs an {@code XmlAdaptedPerson} with the given person details.
      */
     public XmlAdaptedPerson(String name, String phone, String email, String address, List<XmlAdaptedReport> reports,
-                            List<XmlAdaptedMedHistory> medHistories, List<XmlAdaptedAppt> appts, List<XmlAdaptedTag> tagged) {
+                            List<XmlAdaptedMedHistory> medHistories, List<XmlAdaptedAppt> appts,
+                            List<XmlAdaptedTag> tagged) {
         this.name = name;
         this.phone = phone;
         this.email = email;
