5af75c995594035d56445503f1dfa8f45bb0a1de
brandonccm1996 <brandonccm@hotmail.com>
2018-10-21 21:12:28 +0800

    Rectified checkstyle errors

--- src/main/java/seedu/address/model/util/DateTimeUtil.java
+++ src/main/java/seedu/address/model/util/DateTimeUtil.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Helper functions for LocalDate and LocalDateTime conversion to and from String
+ */
 public class DateTimeUtil {
     private static final String DATE_PATTERN = "dd-MM-yyyy";
     private static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
@@ -18,17 +21,17 @@ public class DateTimeUtil {
         return DATE_FORMATTER.format(date);
     }
 
-    // convert String to LocalDate
-    public static LocalDate parseDate(String dateString) {
-        return LocalDate.parse(dateString, DATE_FORMATTER);
-    }
-
     // convert LocalDateTime to String
     public static String format(LocalDateTime dateTime) {
         if (dateTime == null) {
             return null;
         }
         return DATETIME_FORMATTER.format(dateTime);
+    }
+
+    // convert String to LocalDate
+    public static LocalDate parseDate(String dateString) {
+        return LocalDate.parse(dateString, DATE_FORMATTER);
     }
 
     // convert String to LocalDateTime
