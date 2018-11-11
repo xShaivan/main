3a743038f13d16e672ed43761b2b9af75cf157ba
brandonccm1996 <brandonccm@hotmail.com>
2018-11-11 16:49:10 +0800

    Rectified checkstyle errors

--- src/test/java/seedu/address/model/appt/ApptTest.java
+++ src/test/java/seedu/address/model/appt/ApptTest.java
@@ -12,7 +12,6 @@ import static seedu.address.testutil.TypicalAppts.APPT_EXAMPLE2;
 
 import org.junit.Test;
 
-import seedu.address.model.person.Address;
 import seedu.address.testutil.ApptBuilder;
 import seedu.address.testutil.Assert;
 
@@ -21,8 +20,8 @@ public class ApptTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        Assert.assertThrows(NullPointerException.class,
-                () -> new Appt(null, null, null, null, null));
+        Assert.assertThrows(NullPointerException.class, () ->
+                new Appt(null, null, null, null, null));
     }
 
     @Test
